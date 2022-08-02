81aae00655f0078c6659d0c1e8c360c8afa23347
Shayan <Shayanheydari1121@gmail.com>
2022-08-03 00:07:25 +0430

    Edite Docstring

--- Cls/watch.py
+++ Cls/watch.py
@@ -7,17 +7,27 @@ import jdatetime
 from random import randint
 
 class Watch:
+    
     def RandInt()-> int:
+
+        """ Create number random for color random """
+
         x = randint(0, 255)
         y = randint(0, 255)
         z = randint(0, 255)
         return x,y,z
         
     def Time()-> str:
+
+        """ return time """
+
         NewTime = jdatetime.datetime.now().strftime("shayan->\n%a, %d %b \n %Y %H:%M")
         return NewTime
 
     def GetPhoto():
+
+        """ Create Image and Save with name wathsave.jpg"""
+
         img = Image.open(r'Cls/watch.jpg')
         I1 = ImageDraw.Draw(img)
         MyFont = ImageFont.truetype('FreeMono.ttf', 12)
